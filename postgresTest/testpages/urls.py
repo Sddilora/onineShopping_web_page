813e540b77ac44ab75f80de96d112d6d4c86fc0e
Sddilora <sumeyyedilaradogan@gmail.com>
2023-04-17 00:14:03 +0300

    user interface updated example imgs added

--- postgresTest/testpages/urls.py
+++ postgresTest/testpages/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     # path("", views.index, name="index"),
     path("mypage/", views.mypage, name="testpages/mypage"),
-    # path("logout/", views.logout, name="logout"),
+    # path("add-product/", views.product_list, name="product_list"),
     # path("callback/", views.callback, name="callback"),
 ]
--- postgresTest/testpages/views.py
+++ postgresTest/testpages/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+# from .models import Product
+# from .forms import ProductForm
 
 # Create your views here.
 def mypage(request):
     return render(request, "mypage.html", context={ "session": request.session.get("user") })
+
+# def product_list(request):
+#     user_info = request.session.get("user")
+#     products = Product.objects.filter(user=request.user)
+#     if request.method == 'POST':
+#         form = ProductForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             product = form.save(commit=False)
+#             product.user = request.user
+#             product.save()
+#     else:
+#         form = ProductForm()
+#     context = {
+#         'form': form,
+#         'products': products,
+#         'user_info': user_info
+#     }
+#     return render(request, 'product_list.html', context)
+
